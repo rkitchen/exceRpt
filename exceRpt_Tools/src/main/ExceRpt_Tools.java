af2d02dc0f32d63d7deba437d3b40681281f2185
Rob Kitchen <rob.kitchen@yale.edu>
2015-11-09 16:31:21 -0500

    Update to include new adapter detection code in Thunder

--- exceRpt_Tools/src/main/ExceRpt_Tools.java
+++ exceRpt_Tools/src/main/ExceRpt_Tools.java
@@ -21,7 +21,7 @@ import fastqTools.RemoveHomopolymers;
 
 public class ExceRpt_Tools {
 
-	public static final String VERSION = "0.5.7";
+	public static final String VERSION = "1.0.1";
 	
 	public static final String OPT_PATH_DB_ANNOTATION = "A";
 	public static final String OPT_PATH_DB_SAMPLE = "S";
