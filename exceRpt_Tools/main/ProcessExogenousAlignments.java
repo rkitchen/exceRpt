f0970004a178c203563ed05de99b971935432204
Rob Kitchen <rob.kitchen@yale.edu>
2016-09-21 12:17:46 +0100

    Taxonomy assignment for exogenous rRNA alignments
    makefile:
    - now sorts and resolves RDP alignments against the NCBI taxonomy
    - rRNA taxa counts now included in the CORE RESULTS
    
    exceRpt_Tools:
    - added -rdp option to parse alignments against the NCBI taxonomy

--- exceRpt_Tools/main/ProcessExogenousAlignments.java
+++ exceRpt_Tools/main/ProcessExogenousAlignments.java
@@ -33,10 +33,18 @@ public class ProcessExogenousAlignments {
 	 * @param alignmentsPath
 	 * @throws IOException
 	 */
-	public ProcessExogenousAlignments(boolean topDown, double minFraction, int verbose) throws IOException{ 
+	public ProcessExogenousAlignments(int dataType, boolean topDown, double minFraction, int minReads, int verbose) throws IOException{ 
 		_verbose = verbose; 
+		_minReads = minReads;
 		_topDownSearch = topDown;
 		_minFraction = minFraction;
+		_thisDataType = dataType;
+
+		//System.err.println("_verbose = "+_verbose+"\t_verbose == 2: "+(_verbose == 2));
+
+		// if these alignments are from RDP, we have to to top-down as the leaf nodes are not guaranteed to be the ones with the read alignments
+		//if(dataType == DATA_TYPE_RRNA)
+		//	_topDownSearch = true;
 	}
 
 
@@ -75,8 +83,8 @@ public class ProcessExogenousAlignments {
 
 			int inputReadNumber = _reads2species.size();
 			int minReads = (int)Math.floor(_minFraction * (inputReadNumber+0.0));
-			
-			
+
+
 			IO_utils.printLineErr("Processing alignments in batch "+batchCount);
 			//System.err.println(" (batch "+batchCount+")");
 			if(_countIgnoredReads)
@@ -86,13 +94,14 @@ public class ProcessExogenousAlignments {
 
 
 
-			// top down
+
 			if(_topDownSearch){
+				// top down - slow but complete
 				//processAlignments(_taxonomy.getRootNode());
 				processAlignments(_taxonomy.getNode("cellular organisms"));
 				//processAlignments(_taxonomy.getNode("bacteria"));
 			}else{
-				// bottom up
+				// bottom up - fast (default)
 				processAlignments_bottomUP(new HashSet<String>(), minReads);
 				IO_utils.printLineErr(" - Done. Suppressed "+_reads2species.size()+" total reads ("+(Math.round((_reads2species.size()+0.0)*100000.0/(inputReadNumber+0.0))/1000.0)+"% of input reads) due to minimum read requirement.");
 			}
@@ -113,14 +122,20 @@ public class ProcessExogenousAlignments {
 		countReads();
 		//IO_utils.printLineErr("Quantified taxonomy:");
 		String indent = ">";
-		int minReads = 3;
-		_taxonomy.getRootNode().printSummary(indent, minReads);
+		_taxonomy.getRootNode().printSummary(indent, _minReads);
 
 		IO_utils.printLineErr("Done");
 	}
 
 
 	private String _lastReadID=null, _lastSpeciesID=null;
+
+	private int _minReads = 3;
+
+	private int _thisDataType = 0;
+	private static final int DATA_TYPE_GENOME = 0;
+	private static final int DATA_TYPE_RRNA = 1;
+
 	/**
 	 * 
 	 * @param alignmentsPath where to 
@@ -145,10 +160,20 @@ public class ProcessExogenousAlignments {
 		while((line=_alignmentReader.readLine())!=null){
 			hasMoreAlignments = true;
 
-			String[] bits = line.split("\t");
+			String readID = "";
+			String species = "";
 
-			String readID = bits[0].trim();
-			String species = bits[2].replace("_", " ").toLowerCase().trim();
+			if(_thisDataType == DATA_TYPE_GENOME){
+				String[] bits = line.split("\t");
+				readID = bits[0].trim();
+				//String species = bits[2].replace("_", " ").toLowerCase().trim();
+				species = NCBITaxonomy_Engine.sanitiseName(bits[2]);				
+			}
+			else if(_thisDataType == DATA_TYPE_RRNA){
+				String[] bits = line.split(" ");
+				readID = bits[0].trim();
+				species = findMatchToTaxonomy(bits[1].trim());
+			}
 
 			//if(count < 10){
 			//System.out.println(_lastReadID+"\t"+readID+"\t"+line);
@@ -181,6 +206,88 @@ public class ProcessExogenousAlignments {
 	}
 
 
+
+	/**
+	 * Specialist function to search for the appropriate NCBI taxonomy ID from the provided RDP ID.
+	 * 
+	 * @param speciesStringRDP
+	 * @return
+	 */
+	private String findMatchToTaxonomy(String speciesStringRDP){
+		//boolean matchedID = false;
+		String matchedTaxaID = null;
+
+		// split the RDP ID into the main and lineage parts
+		String[] mainBits = speciesStringRDP.split("Lineage=");
+		String firstPart = NCBITaxonomy_Engine.sanitiseName(mainBits[0]);
+		//String firstPart = mainBits[0].replaceAll("_", " ").replaceAll("\"", "").toLowerCase().trim();
+		firstPart = firstPart.split(";")[0].trim();
+		//System.out.println("firstPart = "+firstPart);
+
+		// look for a taxa match to the first part of the RDP ID (preferred)
+		if(_taxonomy.containsNode(firstPart)){
+			// if the main ID is in the taxonomy, use it:
+			//System.out.println(firstPart+"\t"+_taxonomy.containsNode(firstPart));
+			matchedTaxaID = firstPart;
+		}
+
+		// if the main ID is NOT in the taxonomy, try different substrings of it, e.g. remove the first section that is usually an RDP specific ID:
+		if(matchedTaxaID == null){
+			String[] tmpBits2 = firstPart.split(" ");
+			if(tmpBits2.length >= 2){
+				// try just removing the first part of the ID
+				String toTry = tmpBits2[1];
+				for(int i=2;i<tmpBits2.length;i++)
+					toTry = toTry.concat(" "+tmpBits2[i]);
+				//System.out.println(">>> "+toTry+"\t"+_taxonomy.containsNode(toTry));
+				if(_taxonomy.containsNode(toTry)){
+					matchedTaxaID = toTry;
+				}
+			}
+		}
+
+
+		if(matchedTaxaID != null){
+			//System.out.println("FOUND:\t"+matchedTaxaID+"\t"+_taxonomy.getNode(matchedTaxaID).getLevel());
+		}
+
+
+		//
+		// FINALLY just check the Lineage string in case there is a better description here (search string from end to start to find the best possible match)
+		//
+		String matchedID_lineage = null;
+		String[] tmpBits = mainBits[1].split(";");
+		for(int i=tmpBits.length-1;i>0;i--){
+			String tmpID = tmpBits[i].toLowerCase();
+			tmpID = tmpID.replaceAll("\"", "");
+			tmpID = tmpID.replaceAll("_", " ");
+			if(_taxonomy.containsNode(tmpID)){
+				//System.out.println(tmpID+"\t"+_taxonomy.containsNode(tmpID));
+				matchedID_lineage = tmpID;
+				break;
+			}
+		}
+
+		if(matchedID_lineage != null){
+			//System.out.println("FOUND:\t"+matchedID_lineage+"\t"+_taxonomy.getNode(matchedID_lineage).getLevel());
+		}
+
+
+		// If the bacteria is just 'uncultured_bacterium', pick the lineage version instead
+		if(matchedTaxaID == null  ||  matchedTaxaID.equals("uncultured bacterium"))
+			matchedTaxaID = matchedID_lineage;
+
+
+		// TODO: compare matchedID_lineage with matchedTaxaID and return the one that is most specific
+
+		//if(matchedTaxaID != null)
+		return matchedTaxaID;
+		//else
+		//	return matchedID_lineage;
+	}
+
+
+
 	private boolean _countIgnoredReads = false; // can cause high memory usage if there are a lot of these
 	HashSet<String> _ignoredReads = new HashSet<String>();
 	/**
@@ -193,26 +300,34 @@ public class ProcessExogenousAlignments {
 		//if(_taxonomy.getNodeName2nodeIndex().containsKey(species)){
 		if(_taxonomy.containsNode(species)){
 
-			// add the species if we haven't seen it before
-			if(!_species2reads.containsKey(species)){
-				_species2reads.put(species, new HashSet<String>());
-				_species2readCounts.put(species, 0);
+			if(_taxonomy.getNode(species).hasChildren()){
+				// if we're adding a read to a non-leaf node, add it to all leaves below this node
+
+				//System.out.println("N children = "+_taxonomy.getNode(species).getAllLeafSpeciesForNode().size());
+				for(String tmp: _taxonomy.getNode(species).getAllLeafSpeciesForNode()){
+					addRead(readID, tmp);
+				}
+			}else{
+				// add the species if we haven't seen it before
+				if(!_species2reads.containsKey(species)){
+					_species2reads.put(species, new HashSet<String>());
+					_species2readCounts.put(species, 0);
+				}
+
+				// add the read if we haven't seen it before
+				if(!_reads2species.containsKey(readID)){
+					_reads2species.put(readID, new HashSet<String>());
+				}
+				_reads2species.get(readID).add(species);
+
+				// if this read is not already assigned to this species, add it and increment the count
+				if(!_species2reads.get(species).contains(readID)){
+					_species2reads.get(species).add(readID);
+					int thisCount = _species2readCounts.get(species)+1;
+					_species2readCounts.put(species, thisCount);
+				}
+
 			}
-
-			// add the read if we haven't seen it before
-			if(!_reads2species.containsKey(readID)){
-				_reads2species.put(readID, new HashSet<String>());
-			}
-			_reads2species.get(readID).add(species);
-
-			// if this read is not already assigned to this species, add it and increment the count
-			if(!_species2reads.get(species).contains(readID)){
-				_species2reads.get(species).add(readID);
-				int thisCount = _species2readCounts.get(species)+1;
-				_species2readCounts.put(species, thisCount);
-			}
-
-
 
 
 
@@ -358,24 +473,29 @@ public class ProcessExogenousAlignments {
 		// try the parent of this node		
 		//if(!thisNode.getParent().getName().equals("root"))
 		//if(thisNode.getParent().getName().equals("root")  ||  reads.size() == 0){
-		if(thisNode.getParent().getName().equals("cellular organisms")  ||  reads.size() == 0){
-			// if we've not yet assigned these reads, assign them all to cellular organisms
-			NCBITaxonomy_Node junkNode = _taxonomy.getNode("cellular organisms");
+		try{
+			if(thisNode.getParent().getName().equals("cellular organisms")  ||  reads.size() == 0){
+				// if we've not yet assigned these reads, assign them all to cellular organisms
+				NCBITaxonomy_Node junkNode = _taxonomy.getNode("cellular organisms");
 
-			// if we've not yet assigned these reads, assign them all to cellular organisms
-			//NCBITaxonomy_Node junkNode = _taxonomy.getNode("root");
+				// if we've not yet assigned these reads, assign them all to cellular organisms
+				//NCBITaxonomy_Node junkNode = _taxonomy.getNode("root");
 
-			if(_verbose == 2)
-				IO_utils.printErr("parent: "+junkNode.getName()+"\tlevel: "+junkNode.getLevel());
-			readIterator = reads.iterator();
-			while(readIterator.hasNext()){
-				String thisRead = readIterator.next();
-				_readAssignment.put(thisRead, junkNode);
-				assignedreads.add(thisRead);
+				if(_verbose == 2)
+					IO_utils.printErr("parent: "+junkNode.getName()+"\tlevel: "+junkNode.getLevel());
+				readIterator = reads.iterator();
+				while(readIterator.hasNext()){
+					String thisRead = readIterator.next();
+					_readAssignment.put(thisRead, junkNode);
+					assignedreads.add(thisRead);
+				}
+				tidyUp(reads, assignedreads);
+			}else{
+				assessNode(thisNode.getParent(), reads, assignedreads);
 			}
-			tidyUp(reads, assignedreads);
-		}else{
-			assessNode(thisNode.getParent(), reads, assignedreads);
+		}catch(NullPointerException e){
+			// TODO figure out why this is happening!
+			//IO_utils.printLineErr("ERROR: NullPointer at node "+thisNode.getName()+" ("+reads.size()+" reads)");
 		}
 	}
 
@@ -510,11 +630,14 @@ public class ProcessExogenousAlignments {
 		options.addOption(OptionBuilder.withArgName("integer").hasArg().withDescription("[optional] process alignments in batches of reads [default: "+DEFAULT_BATCH_SIZE+"]").create("batchSize"));
 		options.addOption(OptionBuilder.withArgName("decimal").hasArg().withDescription("[optional] Rapid-run mode: do not resolve alignments for leaf-nodes with fewer than this % of total reads [default: 0.0] [suggested 0.001]").create("min"));
 		options.addOption(OptionBuilder.withDescription("[optional] Perform tree search \'top down\'.  This is guaranteed to find the correct assignment for each read, but can be very slow for a large number of alignments").create("topDown"));
+		options.addOption(OptionBuilder.withDescription("If the inout alignments are agains exogenous rRNA sequences in the RDP").create("rdp"));
+		options.addOption(OptionBuilder.withArgName("integer").hasArg().withDescription("[optional] output taxa information for branches with at least this number of alignments [default: "+DEFAULT_MINREADS+"]").create("minReads"));
 		options.addOption(OptionBuilder.withDescription("Print verbose status messages").create("v"));
 		options.addOption(OptionBuilder.withDescription("Print REALLY verbose status messages").create("vv"));
 		return options;
 	}
 
+	public static final int DEFAULT_MINREADS = 3;
 	public static final int DEFAULT_BATCH_SIZE = 200000;
 	public int _verbose = 0;
 
@@ -522,32 +645,51 @@ public class ProcessExogenousAlignments {
 		String alignmentsPath;
 		//alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/TCGA-EW-A1P8.normal.unmapped_nonIBC_TCGA-EW-A1P8_ExogenousGenomicAlignments.sorted.unique.txt";
 		//alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/Sample_Yale-IBC-298-normal.unmapped_ExogenousGenomicAlignments.sorted.unique.txt";
-		alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/PG0004515-BLD.hs37d5.unmapped_ExogenousGenomicAlignments.sorted.unique.txt";
+		//alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/PG0004515-BLD.hs37d5.unmapped_ExogenousGenomicAlignments.sorted.unique.txt";
 		//alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/A806WMABXX.unmapped_ExogenousGenomicAlignments.sorted.unique.txt";
-		//alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/test.txt";
+		alignmentsPath = "/Users/robk/WORK/YALE_offline/tmp/Lajos/exogenousTEST/test.txt";
 
 		//alignmentsPath = "/Users/robk/Downloads/TEST.txt";
 		//args = new String[]{"--taxonomyPath","/Users/robk/WORK/YALE_offline/ANNOTATIONS/taxdump", "--alignments",alignmentsPath, "--batchSize","200000", "-v", "-min","0.001"};
 		//args = new String[]{"--taxonomyPath","/Users/robk/WORK/YALE_offline/ANNOTATIONS/taxdump", "--alignments",alignmentsPath, "--batchSize","200000", "-v", "-topDown"};
 
+
+
+		alignmentsPath = "/Users/robk/Downloads/ExogenousRibosomalAlignments.head";
+		//args = new String[]{"--taxonomyPath","/Users/robk/WORK/YALE_offline/ANNOTATIONS/taxdump", "--alignments",alignmentsPath, "--rdp", "--minReads","1", "--batchSize","200000", "--v"};
+
 		CommandLine cmdArgs = ExceRpt_Tools.parseArgs(args, getCmdLineOptions());
 		if(cmdArgs.hasOption("taxonomyPath") && cmdArgs.hasOption("alignments")){
 
 			int verbose = 0;
-			if(cmdArgs.hasOption("v"))
-				verbose = 1;
-			if(cmdArgs.hasOption("vv"))
+			if(cmdArgs.hasOption("vv")){
+				IO_utils.printLineErr("Printing REALLY verbose status messages");
 				verbose = 2;
+			}else if(cmdArgs.hasOption("v")){
+				IO_utils.printLineErr("Printing verbose status messages");
+				verbose = 1;
+			}
 
 			boolean topDown = false;
 			if(cmdArgs.hasOption("topDown"))
 				topDown = true;
 
+			int dataType = DATA_TYPE_GENOME;
+			if(cmdArgs.hasOption("rdp")){
+				dataType = DATA_TYPE_RRNA;
+				//topDown = true;
+			}
+
+			int minReads = DEFAULT_MINREADS;
+			if(cmdArgs.hasOption("minReads")){
+				minReads = Integer.valueOf(cmdArgs.getOptionValue("minReads")).intValue();
+			}
+
 			double minPercentage = 0.0;
 			if(cmdArgs.hasOption("min"))
 				minPercentage = Double.valueOf(cmdArgs.getOptionValue("min")).doubleValue();
 
-			ProcessExogenousAlignments engine = new ProcessExogenousAlignments(topDown, minPercentage/100.0, verbose);
+			ProcessExogenousAlignments engine = new ProcessExogenousAlignments(dataType, topDown, minPercentage/100.0, minReads, verbose);
 
 			int batchSize = DEFAULT_BATCH_SIZE;
 			if(cmdArgs.hasOption("batchSize"))
