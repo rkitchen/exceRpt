7fd4f9786266aba63d540ba1059f321f715e6b28
Rob Kitchen <r.r.kitchen@gmail.com>
2020-02-10 15:01:58 -0500

    v5 update: use BBDuk for adapter removal and read QC filtering - add PhiX read filtering - combine rRNA+UniVec alignment

--- build/CREATE_DATABASES.sh
+++ build/CREATE_DATABASES.sh
@@ -7,51 +7,51 @@ BASE=/tmp/exceRpt
 ## INSTALL DEPENDENCIES
 ##
 PATH_BIN=$BASE/bin
-mkdir -p $PATH_BIN  && cd $PATH_BIN
-
-## exceRpt
-git clone https://github.com/rkitchen/exceRpt.git
-EXE_EXCERPT_TOOLS=$PATH_BIN/exceRpt/exceRpt_Tools.jar
-git clone https://github.com/rkitchen/Thunder.git
-EXE_THUNDER=$PATH_BIN/Thunder/Thunder.jar
-
-## STAR
-git clone https://github.com/alexdobin/STAR.git
-EXE_STAR=$PATH_BIN/STAR/bin/Linux_x86_64/STAR
-
-## Bowtie2
-wget https://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.3.5.1/bowtie2-2.3.5.1-linux-x86_64.zip
-unzip $PATH_BIN/bowtie2-2.3.5.1-linux-x86_64.zip
-rm $PATH_BIN/bowtie2-2.3.5.1-linux-x86_64.zip
-EXE_BOWTIE2=$PATH_BIN/bowtie2-2.3.5.1-linux-x86_64/bowtie2
-
-## samtools
-wget https://downloads.sourceforge.net/project/samtools/samtools/1.9/samtools-1.9.tar.bz2
-tar -xvf $PATH_BIN/samtools-1.9.tar.bz2
-rm $PATH_BIN/samtools-1.9.tar.bz2
-cd $PATH_BIN/samtools-1.9
-./configure
-make
-cd -
-EXE_SAMTOOLS=$PATH_BIN/samtools-1.9/samtools
-
-##  FastQC
-wget http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
-unzip $PATH_BIN/fastqc_v0.11.8.zip
-rm $PATH_BIN/fastqc_v0.11.8.zip
-PATH_FASTQC=$PATH_BIN/FastQC
-
-## BBduk
-wget https://downloads.sourceforge.net/project/bbmap/BBMap_38.73.tar.gz
-tar -xvf $PATH_BIN/BBMap_38.73.tar.gz
-rm $PATH_BIN/BBMap_38.73.tar.gz
-PATH_BBMAP=$PATH_BIN/bbmap
-
-## SRAtoolkit
-wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
-tar -xvf $PATH_BIN/sratoolkit.current-ubuntu64.tar.gz
-rm $PATH_BIN/sratoolkit.current-ubuntu64.tar.gz
-EXE_SRA_TOOLKIT=$PATH_BIN/sratoolkit.2.9.6-1-ubuntu64/bin/fastq-dump
+#mkdir -p $PATH_BIN  && cd $PATH_BIN
+#
+### exceRpt
+#git clone https://github.com/rkitchen/exceRpt.git
+#EXE_EXCERPT_TOOLS=$PATH_BIN/exceRpt/exceRpt_Tools.jar
+#git clone https://github.com/rkitchen/Thunder.git
+#EXE_THUNDER=$PATH_BIN/Thunder/Thunder.jar
+#
+### STAR
+#git clone https://github.com/alexdobin/STAR.git
+#EXE_STAR=$PATH_BIN/STAR/bin/Linux_x86_64/STAR
+#
+### Bowtie2
+#wget https://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.3.5.1/bowtie2-2.3.5.1-linux-x86_64.zip
+#unzip $PATH_BIN/bowtie2-2.3.5.1-linux-x86_64.zip
+#rm $PATH_BIN/bowtie2-2.3.5.1-linux-x86_64.zip
+#EXE_BOWTIE2=$PATH_BIN/bowtie2-2.3.5.1-linux-x86_64/bowtie2
+#
+### samtools
+#wget https://downloads.sourceforge.net/project/samtools/samtools/1.9/samtools-1.9.tar.bz2
+#tar -xvf $PATH_BIN/samtools-1.9.tar.bz2
+#rm $PATH_BIN/samtools-1.9.tar.bz2
+#cd $PATH_BIN/samtools-1.9
+#./configure
+#make
+#cd -
+#EXE_SAMTOOLS=$PATH_BIN/samtools-1.9/samtools
+#
+###  FastQC
+#wget http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip
+#unzip $PATH_BIN/fastqc_v0.11.8.zip
+#rm $PATH_BIN/fastqc_v0.11.8.zip
+#PATH_FASTQC=$PATH_BIN/FastQC
+#
+### BBduk
+#wget https://downloads.sourceforge.net/project/bbmap/BBMap_38.73.tar.gz
+#tar -xvf $PATH_BIN/BBMap_38.73.tar.gz
+#rm $PATH_BIN/BBMap_38.73.tar.gz
+#PATH_BBMAP=$PATH_BIN/bbmap
+#
+### SRAtoolkit
+#wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+#tar -xvf $PATH_BIN/sratoolkit.current-ubuntu64.tar.gz
+#rm $PATH_BIN/sratoolkit.current-ubuntu64.tar.gz
+#EXE_SRA_TOOLKIT=$PATH_BIN/sratoolkit.2.9.6-1-ubuntu64/bin/fastq-dump
 
 
 
